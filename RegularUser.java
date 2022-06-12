ebd6e55427bd6fd5b534df05d687a359485c6c36
Mgalperen <85412359+Mgalperen@users.noreply.github.com>
2022-06-12 18:13:48 +0300

    Update RegularUser.java

--- RegularUser.java
+++ RegularUser.java
@@ -9,19 +9,6 @@ public class RegularUser {
     private String email;
     private String password;
 
-    SLNode<Movie> favourities;
-
-    static class SLNode<E>
-    {
-        SLNode<E> []links;
-        E data;
-        SLNode (int m ,E data)
-        {   
-            links = (SLNode<E>[]) new SLNode[m];
-            this.data = data;
-        }
-    }
-
     public String getName() {
 		return name;
 	}
