98fa1e6f56be93eb6bd0e21fc7848639d3590f98
Mgalperen <85412359+Mgalperen@users.noreply.github.com>
2022-06-12 18:04:34 +0300

    Update RegularUser.java

--- RegularUser.java
+++ RegularUser.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class RegularUser {
 
     private String name;
-    private String surnmame;
+    private String surname;
     private int age;
     private String email;
     private String password;
@@ -21,6 +21,38 @@ public class RegularUser {
             this.data = data;
         }
     }
+
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 
     
 
