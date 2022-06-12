5aba4b3d24702ea71019d94e8e46aebb785ba62c
Mgalperen <85412359+Mgalperen@users.noreply.github.com>
2022-06-12 17:46:53 +0300

    Update RegularUser.java

--- RegularUser.java
+++ RegularUser.java
@@ -1,13 +1,20 @@
+
 import java.util.List;
 
 public class RegularUser {
 
-<<<<<<< HEAD
-   private String name;
-=======
-    Queue<Movie> br;
-    // imdatttttttttttt -eda
->>>>>>> ef7deccfe65f0ceb52354adac8ee8ce5cc227487
 
-private String ddd;
+
+    static class SLNode<E>
+    {
+        SLNode<E> []links;
+        E data;
+        SLNode (int m ,E data)
+        {   
+            links = (SLNode<E>[]) new SLNode[m];
+            this.data = data;
+        }
+    }
+
+
 }
